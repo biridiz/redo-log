455fecdb48ba76e8024c116eb78cb9c01ae8447a
biridiz <luiz.dendena@gmail.com>
2022-11-26 10:55:55 -0300

    create tables and insert values of metadata file

--- src/db.js
+++ src/db.js
@@ -1,49 +1,78 @@
 const { Sequelize } = require('sequelize');
 const data = require('../storage/metadata.json');
 const sequelize = new Sequelize('sqlite::memory:');
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-    const tables = Object.keys(data);
-    const fields = [];
-    const querys = [];
-    let nameTablesStringQuery = '(';
-    for (const table of tables) {
-      fields.push(Object.keys(data[table]));
+
+const DatabaseConnection = async () => {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+}
+
+const CreateTables = async () => {
+  const tables = Object.keys(data);
+  const fields = [];
+  const querys = [];
+  let nameTablesStringQuery = '(';
+  for (const table of tables) {
+    fields.push(Object.keys(data[table]));
+  }
+  for (let i=0; i<tables.length; i++) {
+    querys.push(`CREATE TABLE ${tables[i]}`);
+    for (let j=0; j<fields[i].length; j++) {
+      if (j === 0) {
+        nameTablesStringQuery += `${fields[i][j]}`;
+      } else {
+        nameTablesStringQuery += `,${fields[i][j]}`;
+      }
     }
-    for (let i=0; i<tables.length; i++) {
-      querys.push(`CREATE TABLE ${tables[i]}`);
-      for (let j=0; j<fields[i].length; j++) {
-        if (j === 0) {
-          nameTablesStringQuery += `${fields[i][j]}`;
+    nameTablesStringQuery += ');';
+    querys[i] += nameTablesStringQuery;
+  }
+  for (const query of querys) {
+    await sequelize.query(query);
+  }
+}
+
+const InsertValues = async () => {
+  const tables = Object.keys(data);
+  const querys = [];
+  const values = [];
+  let queryIndex= 0;
+  let valueTablesStringQuery;
+
+  for (const table of tables) {
+    const fields = Object.keys(data[table]);
+    for (const field of fields) {
+      values.push(data[table][field])
+    }
+    for (let j=0; j<values[0].length; j++) {
+      querys.push(`INSERT INTO ${table} values `);
+      valueTablesStringQuery = '';
+      for (let i=0; i<values.length; i++) {
+        if (i === 0) {
+          valueTablesStringQuery += `(${values[i][j]}`;
         } else {
-          nameTablesStringQuery += `,${fields[i][j]}`;
+          valueTablesStringQuery += `,${values[i][j]}`;
         }
       }
-      nameTablesStringQuery += ');';
-      querys[i] += nameTablesStringQuery;
+      valueTablesStringQuery += ');';
+      querys[queryIndex] += valueTablesStringQuery;
+      queryIndex++;
     }
     for (const query of querys) {
-      const [results, metadata] = await sequelize.query(query);
-      console.log(results);
-      console.log(metadata);
+      await sequelize.query(query);
     }
-
-    const q = [];
-    let s = '(';
-
-    for (const table of tables) {
-      q.push(`INSERT INTO ${table} values`)
-      const f = Object.keys(data[table]);
-      for (field of f) {
-        console.log(field)
-        console.log(data[table][field]);
-
-      }
-      console.log(q)
+    const [results] = await sequelize.query(`SELECT * FROM ${table};`);
+    if (!results?.length) {
+      console.log('Unable to insert values')
     }
+  }
+}
 
+(async () => {
+  try {
+    await DatabaseConnection();
+    await CreateTables();
+    await InsertValues();
 
   } catch (error) {
     console.error('Unable to connect to the database:', error);
