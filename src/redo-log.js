cc9acdc2453b352fc250f37d2edd68d941b4284f
biridiz <luiz.dendena@gmail.com>
2022-12-01 10:29:40 -0300

    identifying operations to be performed

--- src/redo-log.js
+++ src/redo-log.js
@@ -1,31 +1,57 @@
 const fs = require('fs');
 
-// As transações que devem realizar o redo, são todos os commits dos checkpoints
-// e todos os commits após a linha do checkpoint
-
 try {
   const data = fs.readFileSync('../log/db.log', 'utf8');
   const lines = data.split('\n');
   const checkpoints = [];
   const commits = [];
+  const operationsAlreadyPerformed = [];
+  let checkpointLine = 0;
+  let lineNumber = 0;
 
   for (const line of lines) {
     if (line.includes('CKPT')) {
-      checkpoints.push(line.match(/(\w+)/g));
+      checkpoints.push(line.match(/(\w+)/g).slice(1));
+      checkpointLine = lineNumber;
     }
-
     if (line.includes('commit')) {
-      commits.push(line.replace('<', '').replace('>', '').split(' '));
+      commits.push(line.replace('<', '').replace('>', '').replace('commit ', ''));
+    }
+    lineNumber++;
+  }
+
+  for (let i=0; i<checkpoints.length; i++) {
+    for (let j=0; j<checkpoints[i].length; j++) {
+      if (commits.includes(checkpoints[i][j])) {
+        // Econtrou o nome da operação
+        // Agora percorre até encontrar qual operação executar,
+        // interpreta a linha e atualiza o banco
+        operationsAlreadyPerformed.push(checkpoints[i][j]);
+      }
     }
   }
 
-  // identifcar qual commit esta no checkpoint, encontrar e executar sua operação
-  // verificar se alguma operação deve ser executada após o checkpoint
+  // percorre a partir do checkpoint, se encontrar um commit
+  // deve buscar a operação, interpretar a linha e atualizar o banco
+  // IMPORTANTE - ignorar operaçãoes já feitas
 
-  console.log(checkpoints);
-  console.log(commits);
+  for (let i=checkpointLine; i<lines.length; i++) {
+    if (lines[i].includes('commit')) {
+      const op = lines[i].replace('<', '').replace('>', '').replace('commit ', '')
+      if (!operationsAlreadyPerformed.includes(op)) {
+        // Econtrou o nome da operação
+        // Agora percorre até encontrar qual operação executar,
+        // interpreta a linha e atualiza o banco
+        operationsAlreadyPerformed.push(op);
+      }
+    }
+  }
 
-  // console.log(lines)
+  console.log('CKPT', checkpoints);
+  console.log('Commits', commits);
+  console.log('checkpoint lines', checkpointLine);
+  console.log('op executadas', operationsAlreadyPerformed);
+
 } catch (err) {
   console.error(err);
 }
